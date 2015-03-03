5f2fae25a79bed1fc751ffcdbae153c09f717e89
Oscar <Oscar@Demon-PC>
2015-03-03 14:26:04 +0100

    minas y alrededores

--- src/buscaminas/Tablero.java
+++ src/buscaminas/Tablero.java
@@ -1,26 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package buscaminas;
 
-/**
- *
- * @author Oscar
- */
 public class Tablero {
 
     private static final int TAM = 9; //Tamaño de los arrays
-    private String[] col = new String[TAM]; //Array de columnas
-    private String[] fil = new String[TAM]; //Array de filas
-    private String[][] tab = new String[fil.length][col.length]; //Matriz tablero
+    private int[] col = new int[TAM]; //Array de columnas
+    private int[] fil = new int[TAM]; //Array de filas
+    private int[][] tab = new int[fil.length][col.length]; //Matriz tablero
 
     public void crear() {
         // creamos el tablero inicializando todos los valores de la matriz a "0"
         for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab[i].length; j++) {
-                tab[i][j] = "0";
+                tab[i][j] = 0;
             }
         }
     }
@@ -37,12 +28,44 @@ public class Tablero {
     public void minas() {
         // genera de manera aleatoria las posiciones en la matriz de 10 minas
         for (int i = 0; i <= 10; i++) {
-            int posM1 = (int) (Math.random() * 9);
-            int posM2 = (int) (Math.random() * 9);
+            int posM1 = (int) (Math.random() * (TAM - 2)) + 1;
+            int posM2 = (int) (Math.random() * (TAM - 2)) + 1;
             System.out.println(posM1 + " " + posM2);
-            tab[posM1][posM2] = "x";
-            //Hay que buscar la manera de que no se repitan las posiciones, no me voy a rayar por el momento
-        }
-    }
+            tab[posM1][posM2] = 9; //MINA = 9
+            if (tab[posM1 + 1][posM2] != 9) {
+                tab[posM1 + 1][posM2] = tab[posM1 + 1][posM2] + 1;
+            }
 
+            if (tab[posM1 - 1][posM2] != 9) {
+                tab[posM1 - 1][posM2] = tab[posM1 - 1][posM2] + 1;
+            }
+
+            if (tab[posM1][posM2 + 1] != 9) {
+                tab[posM1][posM2 + 1] = tab[posM1][posM2 + 1] + 1;
+            }
+
+            if (tab[posM1][posM2 - 1] != 9) {
+                tab[posM1][posM2 - 1] = tab[posM1][posM2 - 1] + 1;
+            }
+
+            if (tab[posM1 + 1][posM2 - 1] != 9) {
+                tab[posM1 + 1][posM2 - 1] = tab[posM1 + 1][posM2 - 1] + 1;
+            }
+
+            if (tab[posM1 + 1][posM2 + 1] != 9) {
+                tab[posM1 + 1][posM2 + 1] = tab[posM1 + 1][posM2 + 1] + 1;
+            }
+
+            if (tab[posM1 - 1][posM2 - 1] != 9) {
+                tab[posM1 - 1][posM2 - 1] = tab[posM1 - 1][posM2 - 1] + 1;
+            }
+
+            if (tab[posM1 - 1][posM2 + 1] != 9) {
+                tab[posM1 - 1][posM2 + 1] = tab[posM1 - 1][posM2 + 1] + 1;
+            }
+        }
+
+            //Hay que buscar la manera de que no se repitan las posiciones, no me voy a rayar por el momento
+        //EDIT vale se supone qe con todos estos if podemos cambiar los numeros alrededor de las minas para que muestren el numero de minas con las qe estan en contacto pero creo qe falla en algo.
+    }
 }
