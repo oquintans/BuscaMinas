b190cb2a244634f1a3bfb2fdfb1d85b9a29b49f1
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2015-03-25 08:27:03 +0100

    Finalizado

--- src/buscaminas/Main.java
+++ src/buscaminas/Main.java
@@ -8,14 +8,34 @@ public class Main {
 
     public static void main(String[] args) {
         JDialog.setDefaultLookAndFeelDecorated(true);
-        ScoreTime time = new ScoreTime();
         Ficheros fich = new Ficheros();
-        ImageIcon bomba = new ImageIcon(Tablero.class.getResource("/imagenes/boom.png"));
         int option;
+        int op;
         do {
-            option = JOptionPane.showOptionDialog(null, "Selecciona una opción", "gameMenu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,Tablero.icono, new Object[]{"Empezar juego", "Mostrar highScore", "Salir"}, "Empezar juego");
+            option = JOptionPane.showOptionDialog(null, "Selecciona una opción", "gameMenu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, Tablero.icono, new Object[]{"Empezar juego", "Mostrar highScore", "Salir"}, "Empezar juego");
             switch (option) {
                 case 0:
+                    do {
+                        op = JOptionPane.showOptionDialog(null, "Selecciona una opción", "gameMenu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, Tablero.icono, new Object[]{"Facil", "Medio", "Dificil", "Exit"}, "Exit");
+                        if (op == 0) {
+                            Tablero.dificultad = "Facil";
+                            Tablero.NMINAS = 10;
+                            Tablero.TAM = 10;
+                            break;
+                        } else if (op == 1) {
+                            Tablero.dificultad = "Medio";
+                            Tablero.NMINAS = 30;
+                            Tablero.TAM = 15;
+                            break;
+                        } else if (op == 2) {
+                            Tablero.dificultad = "Dificil";
+                            Tablero.NMINAS = 60;
+                            Tablero.TAM = 20;
+                            break;
+                        } else {
+                            System.exit(0);
+                        }
+                    } while (op != 3);
                     Tablero tab = new Tablero();
                     tab.crear();
                     tab.minas();
@@ -23,14 +43,30 @@ public class Main {
                     break;
                 case 1:
                     //Tablero bestScore
-                    fich.ordenar();
-                    fich.visualizar();
+                    do {
+                        op = JOptionPane.showOptionDialog(null, "Selecciona una opción", "gameMenu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, Tablero.icono, new Object[]{"Facil", "Medio", "Dificil", "Exit"}, "Exit");
+                        if (op == 0) {
+                            fich.ordenar("facil");
+                            fich.visualizar("facil");
+                            break;
+                        } else if (op == 1) {
+                            fich.ordenar("medio");
+                            fich.visualizar("medio");
+                            break;
+                        } else if (op == 2) {
+                            fich.ordenar("dificil");
+                            fich.visualizar("dificil");
+                            break;
+                        } else {
+                            System.exit(0);
+                        }
+                    } while (op != 3);
+
                     break;
                 case 2:
                     if (option == -1 && option == 2 && option == 0) {
                         System.exit(0);
                     }
-
             }
         } while (option != -1 && option != 2 && option != 0);
     }
